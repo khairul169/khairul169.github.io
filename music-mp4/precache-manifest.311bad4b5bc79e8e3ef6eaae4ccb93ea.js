3aa4008ba2e32087a8bd261869217ebcea14ab09
Khairul Hidayat <khairulcyber4rt@gmail.com>
2020-05-29 21:10:42 +0700

    Added Music MP4 web build

---
+++ music-mp4/precache-manifest.311bad4b5bc79e8e3ef6eaae4ccb93ea.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "748e626a2b2a8c16010987b1412f6969",
+    "url": "/music-mp4/index.html"
+  },
+  {
+    "revision": "e74d72756dcfd3468835",
+    "url": "/music-mp4/static/css/2.9ff99f11.chunk.css"
+  },
+  {
+    "revision": "cfa9093e8ca695212723",
+    "url": "/music-mp4/static/css/main.ff727311.chunk.css"
+  },
+  {
+    "revision": "e74d72756dcfd3468835",
+    "url": "/music-mp4/static/js/2.27ccce7d.chunk.js"
+  },
+  {
+    "revision": "bab979f0996c4f83a712fc8810462bad",
+    "url": "/music-mp4/static/js/2.27ccce7d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cfa9093e8ca695212723",
+    "url": "/music-mp4/static/js/main.94949277.chunk.js"
+  },
+  {
+    "revision": "1b310944bf2ed580a3ec",
+    "url": "/music-mp4/static/js/runtime-main.e7eec74e.js"
+  },
+  {
+    "revision": "658e85067a9d3ef7ecb1a02025626406",
+    "url": "/music-mp4/static/media/graphic-design.658e8506.png"
+  },
+  {
+    "revision": "e4329ecd9d01a3c96d7c33c0d5def38c",
+    "url": "/music-mp4/static/media/music.e4329ecd.png"
+  }
+]);
