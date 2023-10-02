a8e20950970922b4f5dad3ced7926023a0cbd07f
Khairul Hidayat <me@khairul.my.id>
2023-10-02 18:47:05 +0700

    feat: redirect /wa to whatsapp link

--- next.config.js
+++ next.config.js
@@ -1,6 +1,15 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: 'export',
+  // output: 'export',
+  async redirects() {
+    return [
+      {
+        source: '/wa',
+        destination: 'https://wa.me/6289694230818',
+        permanent: true,
+      },
+    ];
+  },
 };
 
 module.exports = nextConfig;
